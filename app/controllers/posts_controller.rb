b0cbe92a19de70d6c938e686859b952a550cdae0
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-30 18:25:41 -0500

    successfully storing posts based on id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,17 @@ class PostsController < ApplicationController
     
     end
 
+    def show
+        @post = Post.find(params[:id])
+    end
+
     def create
-        render plain: params[:post].inspect
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to @post
+    end
+
+    private def post_params
+        params.require(:post).permit(:title, :body)
     end
 end
