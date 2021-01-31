6bf285dd0f491da896176aab2ea4bd86c9a5d62f
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-31 12:47:33 -0500

    allowing for editing posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,16 @@ class PostsController < ApplicationController
     def edit
         @post = Post.find(params[:id])
     end
-    
+
+    def update
+        @post = Post.find(params[:id])
+        if (@post.update(post_params))
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
     def create
         @post = Post.new(post_params)
         if (@post.save)
