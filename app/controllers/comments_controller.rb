a90d1642008be56caba98dacc28e4416de64167d
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-31 13:36:10 -0500

    little blog system with http auth with user sign in

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,4 +8,11 @@ class CommentsController < ApplicationController
     private def comment_params
         params.require(:comment).permit(:username, :body)
     end
+
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
+    end
 end
